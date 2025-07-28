e24c090cf4f66f5cb4821fde1154cd83c09d6d48
GNR092 <genercan092@gmail.com>
2025-07-28 17:04:27 -0600

    Add packages

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./app/Views/**/*.php",
+    "./app/Controllers/**/*.php",
+    "./app/Helpers/**/*.php",
+    "./public/**/*.html",
+    "./public/**/*.js",
+    "./public/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
