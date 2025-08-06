61c666223723d7689fedb6719ccd36b05ec6baca
GNR092 <genercan092@gmail.com>
2025-08-06 14:29:33 -0600

    Actualizacion de Rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,21 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
+$installerLockFile = WRITEPATH . 'installer.lock';
 
-$routes->get('archivo', 'Archivo::index');
-$routes->post('archivo/subir', 'Archivo::subir');
+if (!file_exists($installerLockFile)) {
 
-$routes->get('modales/(:segment)', 'Modales::mostrar/$1');
+    $routes->get('/', 'Installer::index');
+    $routes->post('installer/process', 'Installer::process');
+    $routes->post('installer/testConnection', 'Installer::testConnection');
+    $routes->get('test', 'Test::index');
 
+} else {
+
+    $routes->get('installer/success', 'Installer::success');
+    $routes->get('/', 'Home::index');
+    $routes->get('archivo', 'Archivo::index');
+    $routes->post('archivo/subir', 'Archivo::subir');
+    $routes->get('modales/(:segment)', 'Modales::mostrar/$1');
+}
