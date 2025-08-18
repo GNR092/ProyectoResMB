19f8cca37e7bd7d8deeea46ca8f2274398bbc997
GNR092 <genercan092@gmail.com>
2025-08-18 10:34:01 -0600

    Fix UsuariosModel

--- app/Models/UsuariosModel.php
+++ app/Models/UsuariosModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UsuarioModels extends Model
+class UsuariosModel extends Model
 {
     protected $table            = 'Usuarios';
     protected $primaryKey       = 'ID_Usuario';
