f4b88f81792b520c6b50719d6f9cecf35ecc9519
RicardoCampos13 <carlos.21070006@itsmotul.edu.mx>
2026-01-12 12:37:58 -0600

    Corección del step en solicitar_material

--- app/Views/modales/solicitar_material.php
+++ app/Views/modales/solicitar_material.php
@@ -407,7 +407,7 @@ $iconUrl = "/icons/icons.svg?v=$version";
                             <input type="text" name="servicio[]" class="w-full px-2 py-1 border rounded" placeholder="Descripción del servicio" required>
                         </td>
                         <td class="px-3 py-2 border">
-                            <input type="number" name="importe[]" class="costo-servicio w-full px-2 py-1 border rounded" min="1" step="1.00" placeholder="0.00" required>
+                            <input type="number" name="importe[]" class="costo-servicio w-full px-2 py-1 border rounded" min="1" step="0.01" placeholder="0.00" required>
                         </td>
                         <td class="px-3 py-2 border text-center">
                             <button type="button" class="eliminar-fila-servicio text-red-600 hover:text-red-800"
