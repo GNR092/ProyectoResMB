e393c63528da23a9d7d8f7c65a7c4cc3864c6a0e
RicardoCampos13 <carlos.21070006@itsmotul.edu.mx>
2026-04-08 17:29:11 -0600

    Update ReportePresupuesto.php

--- app/Views/modales/control/ReportePresupuesto.php
+++ app/Views/modales/control/ReportePresupuesto.php
@@ -598,7 +598,7 @@ $iconUrl = base_url("icons/icons.svg?v=$version");
                             <div class="flex items-center mr-2">                                <label class="inline-flex items-center cursor-pointer">
                                     <input type="checkbox" x-model="reporteDetallado" @change="currentPageVencimientos = 1" class="sr-only peer">
                                     <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-yellow-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-600"></div>
-                                    <span class="ms-3 text-[10px] font-bold text-gray-700 uppercase tracking-tighter">📝 Reporte Detallado</span>
+                                    <span class="ms-3 text-[10px] font-bold text-gray-700 uppercase tracking-tighter">Reporte Detallado</span>
                                 </label>
                             </div>
                             <h2 class="text-xl font-bold text-gray-800">Reportes de Vencimiento</h2>
