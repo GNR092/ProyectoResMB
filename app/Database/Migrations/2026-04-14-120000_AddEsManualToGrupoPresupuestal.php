1d5f53daf169ac5b111f328b66dbc7e3f9278f16
RicardoCampos13 <carlos.21070006@itsmotul.edu.mx>
2026-04-16 13:36:50 -0600

    Create 2026-04-14-120000_AddEsManualToGrupoPresupuestal.php

---
+++ app/Database/Migrations/2026-04-14-120000_AddEsManualToGrupoPresupuestal.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddEsManualToGrupoPresupuestal extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'es_manual' => [
+                'type'       => 'BOOLEAN',
+                'default'    => false,
+                'null'       => false,
+            ],
+        ];
+
+        // En MySQL podemos especificar la posición después de 'activo'
+        if ($this->db->DBDriver === 'MySQLi') {
+            $fields['es_manual']['after'] = 'activo';
+        }
+
+        $this->forge->addColumn('GrupoPresupuestal', $fields);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('GrupoPresupuestal', 'es_manual');
+    }
+}
